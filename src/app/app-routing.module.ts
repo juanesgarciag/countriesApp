162acc31ad00dd590f10f1ddaef15c66af072bde
Juanes <juanesgarciag@gmail.com>
2022-10-04 00:59:48 -0500

    Creación de proyecto, rutas, modulos y componentes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PorPaisComponent } from './paises/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './paises/pages/por-region/por-region.component';
+import { PorCapitalComponent } from './paises/pages/por-capital/por-capital.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PorPaisComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'region',
+    component: PorRegionComponent
+  },
+  {
+    path: 'capital',
+    component: PorCapitalComponent
+  },
+  {
+    path: 'pais/:idPais',
+    component: PorPaisComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaisesModule } from './paises/paises.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PaisesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
